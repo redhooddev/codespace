3137e913de76a23fdc32463a0250b0fc86936b6d
Red Hood Dev <66915205+redhooddev@users.noreply.github.com>
2024-10-24 13:13:04 +0000

    Commit 1

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int x, y, result;
+        x = 6;
+        y = 4;
+
+        result = x + y;
+
+        System.out.println(result);
+    }
+}
+
